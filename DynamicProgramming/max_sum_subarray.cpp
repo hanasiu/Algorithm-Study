a15e5324cef59a4160f126651ece6636b1209ce8
hanasiu <102237333+hanasiu@users.noreply.github.com>
2022-11-01 12:15:49 +0900

    Create max_sum_subarray.cpp

---
+++ DynamicProgramming/max_sum_subarray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int FindMaximumSubarray(const vector<int>& A) {
+	int max_seen = 0, max_end = 0;
+	for (int a : A) {
+		max_end = max(a, a + max_end); // std::max returns the greater of a and b.
+		max_seen = max(max_seen, max_end);
+	}
+	return max_seen;
+}
+
+int main()
+{
+	vector<int> example = { -2,3,1,-1,3,2,-1 };
+	int result = FindMaximumSubarray(example);
+	cout << result;
+	return 0;
+}
