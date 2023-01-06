672976400c88fe9d5358c5ab9a7b633a7021a5d0
hanasiu <102237333+hanasiu@users.noreply.github.com>
2023-01-06 12:09:10 +0900

    Update list.h

--- Linked Lists/list.h
+++ Linked Lists/list.h
@@ -4,25 +4,26 @@
 
 using namespace std;
 
+
 template <typename T>
-struct listNode {
+struct ListNode {
 	T data;
-	shared_ptr<listNode<T>> next;
+	shared_ptr<ListNode<T>> next;
 };
 
-shared_ptr<listNode<int>> searchList(shared_ptr<listNode<int>> L, int key) {
-while (L && L->data != key) {
-	L = L->next;
-}
-return L;
+shared_ptr<ListNode<int>> searchList(shared_ptr<ListNode<int>> L, int key) {
+	while (L && L->data != key) {
+		L = L->next;
+	}
+	return L;
 }
 
-void insertAfter(const shared_ptr<listNode<int>>& node, const shared_ptr<listNode<int>>& new_node) {
+void insertAfter(const shared_ptr<ListNode<int>>& node, const shared_ptr<ListNode<int>>& new_node) {
 	new_node->next = node->next;
 	node->next = new_node;
 }
 
-void deleteAfter(const shared_ptr<listNode<int>>& node) {
+void deleteAfter(const shared_ptr<ListNode<int>>& node) {
 	node->next = node->next->next;
 }
 
